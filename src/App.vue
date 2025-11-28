509bb97d6e4e0fb975892a3146e39d73bbbeb18d
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2025-11-27 19:34:26 -0500

    Taller 15

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <ContadorView />
+    <!-- <ContadorView /> -->
     <!-- <Contador /> -->
+     <DirectivasView/>
   </div>
 </template>
 
 <script>
 import ContadorView from '../src/views/ContadorView.vue'
 // import Contador from "../src/components/Contador.vue"
+import DirectivasView from './views/DirectivasView.vue';
 export default {
 components:{
-  ContadorView,
+  ContadorView,DirectivasView
 },
 };
 </script>
---
+++ src/components/Directivas.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <label for="id_nombre">Nombre</label>
+    <input v-model="nombre" id="id_nombre" type="text" />
+    <p>{{ nombre }}</p>
+
+    <label for="id_apellido">Apellido</label>
+    <input v-model="apellido" id="id_apellido" type="text" />
+    <p>{{ apellido }}</p>
+
+    <!-- <h1>{{ arreglo }}</h1> -->
+
+    <ul>
+        <div>
+        <li v-show="nombre" v-for="{nombre,apellido} in arreglo" :key="nombre">{{nombre}}  {{ apellido }}</li>
+        </div>
+    </ul>
+
+    <h2>Tabla</h2>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-show="nombre && apellido" v-for="{nombre,apellido} in arreglo" :key="nombre">
+               <td>{{nombre}}</td>
+               <td>{{apellido}}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <button v-on:click="imprimirNombre()">Imprimir Nombre</button>
+    <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: null,
+      apellido: null,
+      arreglo: [],
+    };
+  },
+  methods: {
+    imprimirNombre() {
+      console.log(this.nombre);
+    },
+    agregarEstudiante(){
+        const estu = {
+            nombre:this.nombre,
+            apellido:this.apellido
+        }
+        console.log('Se agrega estudiante');
+        console.log(estu);
+        this.arreglo.push(estu);
+        this.limpiarFormulario();
+
+    },
+    limpiarFormulario(){
+        this.nombre=null;
+        this.apellido=null;
+    }
+  },
+};
+</script>
+
+<style>
+</style>
